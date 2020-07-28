edbd6ab00049eececdbb37ed634dd981008bf374
bssughosh <bssughosh27@gmail.com>
2020-07-28 11:33:39 +0530

    Added models for accounts

--- accounts/models.py
+++ accounts/models.py
@@ -5,8 +5,11 @@ from django.db import models
 class UserData(models.Model):
     aadhar = models.BigIntegerField(null=True)
     name = models.CharField(max_length=100)
-    mobile = models.BigIntegerField(max_length=10, null=True)
+    mobile = models.BigIntegerField(null=True)
     add1 = models.CharField(max_length=100, null=True)
     district = models.CharField(max_length=25)
     state = models.CharField(max_length=25)
-    pincode = models.BigIntegerField(max_length=6)
+    pincode = models.BigIntegerField()
+
+    def __str__(self):
+        return self.name
