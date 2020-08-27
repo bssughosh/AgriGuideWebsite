c0b149b33197db637cb25f5fd0166c96dc69c922
Sughosh Sudhanvan <bssughosh27@gmail.com>
2020-08-28 00:24:27 +0530

    Dynamic table with design done

--- weather/views.py
+++ weather/views.py
@@ -4,9 +4,12 @@ from accounts.models import UserData
 
 # Create your views here.
 def show_results(request):
-    user_data = UserData.objects.get(mobile=request.user.get_username())
-    dist = user_data.district
-    state = user_data.state
+    # user_data = UserData.objects.get(mobile=request.user.get_username())
+    # dist = user_data.district
+    # state = user_data.state
+    # TODO: Update the state and dist to User Data Values and input start and end
+    state = ''
+    dist = ''
     start = 6
     end = 10
     return render(request, 'weather/first_page.html', {'state': state, 'dist': dist, 'start': start, 'end': end})
